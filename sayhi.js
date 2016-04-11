97b21a141208ee317eb2ee40327abb58d64d0df8
Anis.MAZLOUT <anis.mazlout@fisglobal.com>
2016-04-11 12:16:03 +0100

    second Hi!!

--- sayhi.js
+++ sayhi.js
@@ -2,6 +2,7 @@ function sayHi (selector) {
     var el = $(selector);
     return function (name) { 
         el.text("Hi " + name + "!");
-        console.log("hiii!!")
+        console.log("hiii!!");
+        console.log("hiii!!");
     }
 }
